91c3041584f63e92fe6928fe0de389285140aac7
Fedor Borshev <f@f213.in>
2018-11-19 21:33:00 +0300

    Ported some old CSS

--- components/AppContent.vue
+++ components/AppContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content" v-html="content" />
+  <div class="app-content" v-html="content" />
 </template>
 <script>
 export default {
@@ -8,3 +8,46 @@ export default {
   },
 };
 </script>
+
+<style>
+.app-content {
+  ul, ol {
+    list-style-type: none;
+    padding-left: 1.5rem;
+
+    li:before {
+      margin-left: -1rem;
+    }
+  }
+
+  ul li:before {
+    content: '\2014\a0';
+  }
+
+  ol {
+    counter-reset: ol;
+    li:before {
+      counter-increment: ol;
+      content: counter( ol ) ')\a0';
+    }
+  }
+}
+
+.app-content {
+  a {
+    text-decoration: none;
+    border-bottom: 1px solid var(--link-border-color);
+    color: var(--link-color);
+    --webkit-tap-highlight-color: rgba( 0, 0, 0, 0 );
+
+    &:hover {
+        color: var(--link-hover);
+        border-color: var(--link-border-hover);
+    }
+
+    &:not(:hover) {
+      transition: color .5s ease, border-color .5s ease;
+    }
+  }
+}
+</style>
--- components/AppPost.vue
+++ components/AppPost.vue
@@ -17,3 +17,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.app-post {
+  margin-bottom: 4rem;
+}
+</style>
