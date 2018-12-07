421e39e96ae58ff48f6f8f5fdd7430fb435cad94
Fedor Borshev <f@f213.in>
2018-12-07 21:50:53 +0300

    Working navigation

--- components/TheMenu.vue
+++ components/TheMenu.vue
@@ -1,41 +1,52 @@
 <template>
   <nav class="app-nav section">
     <div class="container">
-      <nuxt-link to="/" class="nav-link">
-        <span class="nav-link__label">Блог</span>
+      <nuxt-link to="/" active-class="ignored" class="app-nav__link" :class="homePageActive">
+        <span class="app-nav__label">Блог</span>
       </nuxt-link>
-      <nuxt-link to="/favorites/" class="nav-link">
-        <span class="nav-link__label">Избранное</span>
+      <nuxt-link :to="{ name: 'featured' }" class="app-nav__link" active-class="app-nav__link--active">
+        <span class="app-nav__label">Избранное</span>
       </nuxt-link>
-      <nuxt-link to="/me/" class="nav-link">
-        <span class="nav-link__label">Обо мне</span>
+      <nuxt-link to="/me" class="app-nav__link" active-class="app-nav__link--active">
+        <span class="app-nav__label">Обо мне</span>
       </nuxt-link>
-      <nuxt-link to="/books/" class="nav-link">
-        <span class="nav-link__label">Книги</span>
+      <nuxt-link to="/books" class="app-nav__link" active-class="app-nav__link--active">
+        <span class="app-nav__label">Книги</span>
       </nuxt-link>
     </div>
   </nav>
 </template>
+<script>
+export default {
+  computed: {
+    homePageActive() {
+      const { name } = this.$route;
+      return ['index', 'page-number'].includes(name) ? 'app-nav__link--active' : '';
+    },
+  },
+};
+</script>
 
 <style scoped>
 .app-nav {
   padding-top: 1.5rem;
-}
 
-.nav-link {
-  border-bottom: 0;
-  margin-right: 4px;
-  &:not(:first-child):before {
-    content: '·\a0\a0';
-    margin-left: 3px;
-  }
+  &__link {
+    border-bottom: 0;
+    margin-right: 4px;
+    &:not(:first-child):before {
+      content: '·\a0\a0';
+      margin-left: 3px;
+    }
 
-  &.nuxt-link-active {
-    pointer-events: none;
-    .nav-link__label {
-        color: var(--text-color);
-        opacity: .8;
-        border-bottom: 0;
+    &--active
+    {
+      pointer-events: none;
+      .app-nav__label {
+          color: var(--text-color);
+          opacity: .8;
+          border-bottom: 0;
+      }
     }
   }
 
