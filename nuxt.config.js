475e2e4543415a3bff655d6feb4f664a7bb59f57
Fedor Borshev <f@f213.in>
2018-12-11 22:42:48 +0300

    Ya.metrika is working with SPA now

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ module.exports = {
     ],
     __dangerouslyDisableSanitizers: ['script'],
     script: [
-      { innerHTML: '(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter25756085 = new Ya.Metrika2({ id:25756085, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, ecommerce:"dataLayer" }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2");' },
+      { innerHTML: '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(25756085, "init", { id:25756085, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true });' },
     ],
   },
 
@@ -63,7 +63,7 @@ module.exports = {
   */
   plugins: [
     '~/plugins/ghost-auth.js',
-    '~/plugins/ya-metrika.js',
+    { src: '~/plugins/ya-metrika.js', ssr: false },
   ],
 
   /*
@@ -108,7 +108,7 @@ module.exports = {
     absoluteHost: 'https://borshev.com',
     copyrightYears: '2014–2018',
     email: 'fedor@borshev.com',
-    yaMetrikaCounterId: '25756085',
+    yaMetrikaCounterID: '25756085',
   },
 
   /*
