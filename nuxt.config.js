603d24b3aaa70d3a144fe227cd1e6b4cd66b211f
Fedor Borshev <f@f213.in>
2018-12-11 22:28:01 +0300

    some favicon boilerplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 const BACKEND_URL = process.env.BACKEND_URL ? process.env.BACKEND_URL : 'http://localhost:8000';
 
 module.exports = {
@@ -22,7 +23,15 @@ module.exports = {
       { property: 'fb:profile_id', content: '100006565443356' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=2' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' },
+
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans:400,700&amp;subset=cyrillic" rel="stylesheet' },
     ],
     __dangerouslyDisableSanitizers: ['script'],
