238c72626cfe440cde28805bce51ac460a03bf1a
Fedor Borshev <f@f213.in>
2018-12-16 22:36:44 +0300

    Post display mobile fix

--- components/AppPost.vue
+++ components/AppPost.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="app-post">
     <PostTitle class="title is-3 is-size-4-mobile app-post__title" :class="{'is-marginless': withTime}" :post="post" :linked="linked"/>
-    <TimeAgo class="is-size-7 is-hidden-mobile app-post__time" :date="post.created_at" v-if="withTime" />
+    <TimeAgo class="is-size-7 app-post__time" :date="post.created_at" v-if="withTime" />
     <AppContent :content="post.html"/>
     <AppTags class="is-hidden-mobile app-post__tags" :tags="post.tags" />
     <AppShare class="app-post__share" :page="post" />
