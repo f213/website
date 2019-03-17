df063404ef57ef970a0cc17c9da7e595bf344f15
Fedor Borshev <f@f213.in>
2019-03-17 20:42:07 +0300

    Main site navigation updates

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,10 +1,7 @@
 <template>
   <header class="app-header section" ref="container">
     <div class="container app-container">
-      <div class="title is-3">
-        <template v-if="isHomePage">{{ title }}</template>
-        <nuxt-link class="app-header__title-link" v-else to="/">{{ title }}</nuxt-link>
-      </div>
+      <div class="title is-3">{{ title }}</div>
     </div>
   </header>
 </template>
@@ -32,23 +29,6 @@ export default {
 
   @media(width <= 768px) {
     margin-bottom: 4rem;
-  }
-
-  &__title-link {
-    border-bottom: 1px solid rgba(0,0,160,0.15);
-    color: var(--text-color);
-    &:hover {
-        color: var(--link-hover);
-        border-color: var(--link-border-hover);
-    }
-
-    &:not(:hover) {
-      transition: color .5s ease, border-color .5s ease;
-    }
-  }
-
-  &__subtitle {
-    margin-top: -1rem !important;
   }
 }
 </style>
--- components/TheMenu/TheMenuDesktop.vue
+++ components/TheMenu/TheMenuDesktop.vue
@@ -1,7 +1,10 @@
 <template>
   <nav class="section app-nav">
     <div class="container app-container">
-      <HomePageLink class="app-nav__link" label-class="app-nav__label" active-class="app-nav__link--active" />
+      <nuxt-link to="/" class="app-nav__link">
+        <span class="app-nav__label">Главная</span>
+      </nuxt-link>
+      <BlogLink class="app-nav__link" label-class="app-nav__label" active-class="app-nav__link--active" />
       <nuxt-link v-for="(link, index) in links" :key="index" :to="link.to" class="app-nav__link" active-class="app-nav__link--active">
         <span class="app-nav__label">{{ link.label }}</span>
       </nuxt-link>
@@ -10,12 +13,12 @@
   </nav>
 </template>
 <script>
-import HomePageLink from '~/components/TheMenu/HomePageLink.vue';
+import BlogLink from '~/components/TheMenu/BlogLink.vue';
 import TgLink from '~/components/TgLink.vue';
 
 export default {
   components: {
-    HomePageLink,
+    BlogLink,
     TgLink,
   },
   props: {
