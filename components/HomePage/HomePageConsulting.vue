0b8ca229786bd547b921f97ec0a9d71ba567261e
Fedor Borshev <fedor@borshev.com>
2019-09-17 18:45:58 +0300

    Minor consulting updates

--- components/HomePage/HomePageConsulting.vue
+++ components/HomePage/HomePageConsulting.vue
@@ -3,11 +3,10 @@
     <h2 class="title is-3 has-text-weight-normal consulting__header">Делюсь знаниями</h2>
     <p>В режиме один-к-одному делюсь знаниями и отвечаю на вопросы о разработке сложных продуктов.</p>
     <ul class="app-list consulting__list">
+      <li>Как быстро запустить MVP?</li>
       <li>Как построить разработку нового продукта с нуля? Как построить общение с программистами?</li>
       <li>Как сделать, чтобы разработчики не срывали сроки?</li>
-      <li>Какие процессы нужны, когда команда выросла больше 3-х человек?</li>
       <li>Что делать с накопленным техдолгом? Как не создавать новый?</li>
-      <li>Как ускорить time2market?</li>
     </ul>
     <p class="consulting__call-to-action">
       Общаемся по скайпу или через зум с видео. Почитайте <nuxt-link class="consulting__link" to="/consulting/#testimonials">отзывы</nuxt-link> и напишите на <a class="consulting__link" href="mailto:fedor@borshev.com">fedor@borshev.com</a> — договоримся.
--- components/HomePage/HomePageMenu.vue
+++ components/HomePage/HomePageMenu.vue
@@ -4,9 +4,9 @@
       <span class="app-nav__label">Блог</span>
     </nuxt-link>
     <TgLink class="app-nav__link" label-class="app-nav__label" label="Телеграм" />
-    <a href="#talk-to-me" class="app-nav__link">
+    <nuxt-link to="/consulting/" class="app-nav__link">
       <span class="app-nav__label">Консультации</span>
-    </a>
+    </nuxt-link>
   </nav>
 </template>
 <script>
