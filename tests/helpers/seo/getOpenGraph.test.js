0c1710021f2ade1583470b38d9e64a317e945d2a
Fedor Borshev <fedor@borshev.com>
2019-12-31 18:30:29 +0300

    Test revamp (#7)

--- tests/helpers/seo/getOpenGraph.test.js
+++ tests/helpers/seo/getOpenGraph.test.js
@@ -1,47 +1,4 @@
-import { getMeta, getAbsoluteUrl, getOpenGraph } from '~/helpers/seo';
-
-describe('getMeta helper', () => {
-  it('Returns non when there is no meta', () => {
-    const post = { meta_description: null };
-    expect(getMeta(post)).toEqual([]);
-  });
-  it('Returns description when it is present', () => {
-    const post = { meta_description: 'Грузите апельсины бочками' };
-    expect(getMeta(post)[0]).toEqual({
-      hid: 'description',
-      name: 'description',
-      content: 'Грузите апельсины бочками',
-    });
-  });
-  it('Is javascript-proof', () => {
-    const post = null;
-    expect(getMeta(post)).toEqual([]);
-  });
-});
-
-
-describe('getAbsoluteUrl helper', () => {
-  let page;
-
-  beforeEach(() => {
-    process.env.absoluteHost = 'https://test.com';
-    page = {
-      url: '/slug/',
-    };
-  });
-
-  it('Actualy adds the absolute url', () => {
-    expect(getAbsoluteUrl(page)).toEqual('https://test.com/slug/');
-  });
-
-  it('Does not add the host when page already contains it', () => {
-    page = {
-      url: 'https://microsoft.com/money',
-    };
-
-    expect(getAbsoluteUrl(page)).toEqual('https://microsoft.com/money');
-  });
-});
+import { getOpenGraph } from '~/helpers/seo';
 
 describe('getOpenGraph helper', () => {
   let page;
