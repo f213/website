fafc519ebf4859b5de3e3e8c72e2b44eccbe59a5
Fedor Borshev <fedor@borshev.com>
2020-01-02 20:44:32 +0300

    API endpoint for fetch stock ghost AMP posts

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const postPreviewController = require('./api/postPreviewController');
+const postAMPController = require('./api/postAMPController');
 
 const router = express.Router();
 
 router.get('/preview-posts/:uuid', postPreviewController);
+router.get('/amp-posts/:slug', postAMPController);
 
 module.exports = router;
