d924299c709b7ba8755208105a544c3f179dff2b
Fedor Borshev <fedor@borshev.com>
2020-01-02 20:23:27 +0300

    Separate controller for nuxt

--- server/index.js
+++ server/index.js
@@ -5,31 +5,19 @@ require('dotenv').config();
 
 const apiController = require('./controllers/api');
 const ghostController = require('./controllers/ghost');
-const nuxt = require('./nuxt');
-
+const nuxtController = require('./controllers/nuxt');
 
 const redirectToTheMainHostMiddleware = require('./middleware/redirectToMainHostMiddleware');
 
 const app = express();
 
 app.use(redirectToTheMainHostMiddleware);
+
 app.use('/', ghostController);
+app.use('/', nuxtController);
 
 /* V8 is our custom API, not to mess with ghost api, available (proxied) at /api/v2 */
 app.use('/api/v8', apiController);
-
-/* Non-nuxt views */
-app.get('/courses/cto-growth/', (req, res) => res.redirect(302, 'https://pmdaily.ru/courses/cto-growth/'));
-
-
-/* nuxt init */
-if (!process.env.SERVER_ONLY) {
-  consola.info('Running nuxt.js');
-  nuxt({ app });
-} else {
-  consola.warn('Not running nuxt.js because cuz SERVER_ONLY env variable is set');
-}
-
 
 /* Run express */
 const host = process.env.HOST || '127.0.0.1';
