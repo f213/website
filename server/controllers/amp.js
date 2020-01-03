aa3099549fd1b4fb031ecc61c9f9ec1e281bddf7
Fedor Borshev <f@f213.in>
2020-01-03 14:34:07 +0300

    Revert "Revert "SEO -- migration to the ghost AMP generator (#11)" (#12)"
    This reverts commit d3cb2b4e1e94a7b792275a6e495faf540f7576d8.

---
+++ server/controllers/amp.js
@@ -0,0 +1,13 @@
+const { fetchAMPPost } = require('../lib/ghost');
+
+module.exports = async (req, res) => {
+  let html;
+
+  try {
+    html = await fetchAMPPost(req.params.slug);
+  } catch (_) {
+    res.status(404).send({ error: 'Post not found' });
+  }
+
+  res.send(html);
+};
