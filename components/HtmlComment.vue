26d3131a30314c58ba2d20fa9398508464cd1457
Fedor Borshev <fedor@borshev.com>
2020-01-03 20:17:01 +0300

    SEO: Added <!-- noindex --> to the post lists (#14)

---
+++ components/HtmlComment.vue
@@ -0,0 +1,17 @@
+
+<script>
+export default {
+  functional: true,
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+  render(createElement, { props: { text } }) {
+    const vNode = createElement('');
+    vNode.text = text;
+    return vNode;
+  },
+};
+</script>
