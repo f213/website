5bfa21cd38fd20169025bc2bfe0a7a2531928aa0
Fedor Borshev <fedor@borshev.com>
2020-01-03 20:03:04 +0300

    Ported HtmlComment component

---
+++ tests/components/HtmlComment.spec.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import { mount } from '@vue/test-utils';
+import HtmlComment from '~/components/HtmlComment.vue';
+
+describe('Component for rendering HTML comments', () => {
+  it('renders html comment inside tag `div` correctly', () => {
+    const text = 'test';
+    const Root = Vue.extend({
+      components: { HtmlComment },
+      render(h) {
+        return h('div', [
+          h('HtmlComment', { props: { text } }),
+        ]);
+      },
+    });
+    const root = mount(Root);
+    expect(root.html()).toContain('<!--test-->');
+  });
+  it('renders empty comment if text is not specified', () => {
+    const Root = Vue.extend({
+      components: { HtmlComment },
+      render(h) {
+        return h('div', [
+          h('HtmlComment'),
+        ]);
+      },
+    });
+    const root = mount(Root);
+    expect(root.html()).toContain('<!---->');
+  });
+});
