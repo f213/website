a8962d0bcd59fdef732062f540dc4762d3f8dcdc
Fedor Borshev <fedor@borshev.com>
2020-01-08 02:13:30 +0300

    Added fancy subscribe bar to convert traffic from blog to other social media

--- components/AppShare.vue
+++ components/AppShare.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="likely app-share" :data-url="link" ref="container">
     <div class="facebook">Расшарить</div>
-    <div class="twitter">Твитнуть</div>
     <div class="vkontakte">ВК</div>
     <div class="telegram">Телеграм</div>
   </div>
---
+++ components/SubscribeBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="subscribe-bar">
+    <div class="subscribe-bar__image image is-96x96">
+      <img class="is-rounded" src="~/assets/images/me-small.jpg" alt="Фёдор Борщёв">
+    </div>
+    <div class="subscribe-bar__text">
+      <p>Меня зовут Федя Борщёв. Пишу для программистов в <a class="subscribe-bar__link" :href="telegram"><i class="subscribe-bar__telegram-icon fa fa-paper-plane-o" /> телеграме</a> — 3 поста в неделю об управлении сложными проектами, хорошем коде и профессиональном развитии. А ещё я в прямом эфире пишу код на <a class="subscribe-bar__link" :href="youtube"><i class="subscribe-bar__youtube-icon fa fa-youtube-play" /> ютубе</a> и выкладываю сторьки в <a class="subscribe-bar__link" :href="instagram"><i class="fa fa-instagram" /> инстаграм</a>. Подписывайтесь!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      telegram: process.env.telegram,
+      instagram: process.env.instagram,
+      youtube: process.env.youtube,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.subscribe-bar {
+  min-height: 140px;
+
+  &__image {
+    float: left;
+    margin-right: 1rem;
+    position: relative;
+    top: .2rem;
+
+    @media (width > 768px) {
+      width: 128px;
+      height: 128px;
+    }
+  }
+
+  &__text {
+    @media (width > 768px) {
+      padding-top: .8rem;
+    }
+  }
+
+  &__youtube-icon {
+    opacity: .8;
+  }
+
+  &__telegram-icon {
+    opacity: .9;
+  }
+
+  &__link {
+    color: var(--link-color);
+
+    &:hover {
+      color: var(--link-hover);
+    }
+
+    &:not(:hover) {
+      transition: color .5s ease;
+    }
+  }
+}
+</style>
