9fa733f7e01d40677f22d93bcf78e6beae22061a
Vladimir Taranovskiy <8756136@gmail.com>
2020-01-09 00:53:34 +0300

    Added toggle slides on click bullets

--- helpers/mixins/galleryCarousel.js
+++ helpers/mixins/galleryCarousel.js
@@ -11,17 +11,43 @@ export default {
         const slides = gallery.querySelectorAll('.kg-gallery-image img');
         this.addBullets(gallery, slides.length);
         this.toggleSlide(gallery);
+      });
+    },
 
-        let currentSlideIndex = 1;
+    addBullets(gallery, count) {
+      const slides = gallery.querySelectorAll('.kg-gallery-image img');
+      const bullets = document.createElement('ul');
 
-        gallery.addEventListener('click', () => {
-          this.toggleSlide(gallery, currentSlideIndex);
-          if (currentSlideIndex < slides.length - 1) {
-            currentSlideIndex += 1;
-            return;
-          }
-          currentSlideIndex = 0;
+      bullets.classList.add('bullets');
+      gallery.append(bullets);
+
+      for (let i = 0; i < count; i += 1) {
+        const bulletsItem = document.createElement('li');
+        bulletsItem.classList.add('bullets-item');
+        bullets.append(bulletsItem);
+      }
+      const bulletsList = gallery.querySelectorAll('.bullets-item');
+      this.addGalleryEventListeners(gallery, slides, bulletsList);
+    },
+
+    addGalleryEventListeners(gallery, slides, bullets) {
+      let currentSlideIndex = 1;
+
+      bullets.forEach((item, index) => {
+        item.addEventListener('click', (evt) => {
+          evt.stopPropagation();
+          currentSlideIndex = (index < bullets.length - 1) ? index + 1 : 0;
+          this.toggleSlide(gallery, index);
         });
+      });
+
+      gallery.addEventListener('click', () => {
+        this.toggleSlide(gallery, currentSlideIndex);
+        if (currentSlideIndex < slides.length - 1) {
+          currentSlideIndex += 1;
+          return;
+        }
+        currentSlideIndex = 0;
       });
     },
 
@@ -29,29 +55,15 @@ export default {
       const slides = gallery.querySelectorAll('.kg-gallery-image img');
 
       slides.forEach((slide, index) => {
-        slide.classList.add('active');
         slide.classList.remove('active');
         if (index === currentIndex) {
           slide.classList.add('active');
-          this.toggleActiveBullet(gallery, index);
+          this.highlightActiveBullet(gallery, index);
         }
       });
     },
 
-    addBullets(gallery, count) {
-      const bullets = document.createElement('ul');
-
-      bullets.classList.add('bullets');
-      gallery.append(bullets);
-
-      for (let i = 1; i <= count; i += 1) {
-        const bulletsItem = document.createElement('li');
-        bulletsItem.classList.add('bullets-item');
-        bullets.append(bulletsItem);
-      }
-    },
-
-    toggleActiveBullet(gallery, index) {
+    highlightActiveBullet(gallery, index) {
       const bullets = gallery.querySelectorAll('.bullets-item');
       bullets.forEach(item => item.classList.remove('active'));
       bullets[index].classList.add('active');
