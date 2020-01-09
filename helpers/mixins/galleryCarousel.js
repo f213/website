3fbefaa4a25c1740ef777ea4def90faa97de904f
Vladimir Taranovskiy <8756136@gmail.com>
2020-01-09 12:38:06 +0300

    Refactored a galleryCarousel mixin

--- helpers/mixins/galleryCarousel.js
+++ helpers/mixins/galleryCarousel.js
@@ -1,62 +1,59 @@
 export default {
   mounted() {
-    this.addCarouseles();
+    this.initCarouseles();
   },
 
   methods: {
-    addCarouseles() {
+    initCarouseles() {
       const galleries = document.querySelectorAll('.kg-gallery-container');
 
       galleries.forEach((gallery) => {
         const slides = gallery.querySelectorAll('.kg-gallery-image img');
-        this.addBullets(gallery, slides.length);
-        this.toggleSlide(gallery);
+        this.addBullets(gallery, slides, slides.length);
+        this.toggleSlide(gallery, slides);
       });
     },
 
-    addBullets(gallery, count) {
-      const slides = gallery.querySelectorAll('.kg-gallery-image img');
-      const bullets = document.createElement('ul');
+    addBullets(gallery, slides, count) {
+      const bulletsElement = document.createElement('ul');
 
-      bullets.classList.add('bullets');
-      gallery.append(bullets);
+      bulletsElement.classList.add('bullets');
+      gallery.append(bulletsElement);
 
       for (let i = 0; i < count; i += 1) {
         const bulletsItem = document.createElement('li');
         bulletsItem.classList.add('bullets-item');
-        bullets.append(bulletsItem);
+        bulletsElement.append(bulletsItem);
       }
-      const bulletsList = gallery.querySelectorAll('.bullets-item');
-      this.addGalleryEventListeners(gallery, slides, bulletsList);
+      const bullets = gallery.querySelectorAll('.bullets-item');
+      this.addGalleryEventListeners(gallery, slides, bullets);
     },
 
     addGalleryEventListeners(gallery, slides, bullets) {
-      let currentSlideIndex = 1;
+      let activeSlideIndex = 1;
 
       bullets.forEach((item, index) => {
         item.addEventListener('click', (evt) => {
           evt.stopPropagation();
-          currentSlideIndex = (index < bullets.length - 1) ? index + 1 : 0;
-          this.toggleSlide(gallery, index);
+          activeSlideIndex = (index < bullets.length - 1) ? (index + 1) : 0;
+          this.toggleSlide(gallery, slides, index);
         });
       });
 
       gallery.addEventListener('click', () => {
-        this.toggleSlide(gallery, currentSlideIndex);
-        if (currentSlideIndex < slides.length - 1) {
-          currentSlideIndex += 1;
+        this.toggleSlide(gallery, slides, activeSlideIndex);
+        if (activeSlideIndex < slides.length - 1) {
+          activeSlideIndex += 1;
           return;
         }
-        currentSlideIndex = 0;
+        activeSlideIndex = 0;
       });
     },
 
-    toggleSlide(gallery, currentIndex = 0) {
-      const slides = gallery.querySelectorAll('.kg-gallery-image img');
-
+    toggleSlide(gallery, slides, activeSlideIndex = 0) {
       slides.forEach((slide, index) => {
         slide.classList.remove('active');
-        if (index === currentIndex) {
+        if (index === activeSlideIndex) {
           slide.classList.add('active');
           this.highlightActiveBullet(gallery, index);
         }
--- tests/helpers/mixins/galleryCarousel.test.js
+++ tests/helpers/mixins/galleryCarousel.test.js
@@ -17,7 +17,7 @@ const testContent = `
 describe('gallery slides', () => {
   it('After content rendering one slide should be active', () => {
     document.body.innerHTML = testContent;
-    galleryCarousel.methods.addCarouseles();
+    galleryCarousel.methods.initCarouseles();
 
     expect(document.body.querySelectorAll('.kg-gallery-container .kg-gallery-image img.active').length).toBe(1);
 
@@ -26,7 +26,7 @@ describe('gallery slides', () => {
 
   it('On gallery clicks one slide should remain active', () => {
     document.body.innerHTML = testContent;
-    galleryCarousel.methods.addCarouseles();
+    galleryCarousel.methods.initCarouseles();
 
     const gallery = document.querySelector('.kg-gallery-container');
     const slides = document.querySelectorAll('.kg-gallery-image');
@@ -43,7 +43,7 @@ describe('gallery slides', () => {
 
   it('On gallery clicks an active slide should change cyclically', () => {
     document.body.innerHTML = testContent;
-    galleryCarousel.methods.addCarouseles();
+    galleryCarousel.methods.initCarouseles();
 
     const gallery = document.querySelector('.kg-gallery-container');
     const slides = document.querySelectorAll('.kg-gallery-image');
@@ -66,7 +66,7 @@ describe('gallery slides', () => {
 describe('gallery bullets', () => {
   it('Rendered bullets blocks', () => {
     document.body.innerHTML = testContent;
-    galleryCarousel.methods.addCarouseles();
+    galleryCarousel.methods.initCarouseles();
     expect(document.body.querySelectorAll('.bullets').length).toBeGreaterThan(0);
   });
 });
