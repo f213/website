c224924225a9cc4f8a3a493c03a5637d79253ea5
Fedor Borshev <fedor@borshev.com>
2020-01-09 21:25:48 +0300

    Ya.metrika goals for the subscription links

--- plugins/ya-metrika.js
+++ plugins/ya-metrika.js
@@ -1,8 +1,18 @@
+import Vue from 'vue';
+
 import yaMetrika from '~/helpers/yaMetrika';
 
 export default ({ app }) => {
+  /* Send hit after each page change */
   app.router.beforeEach((to, _, next) => {
     yaMetrika.hit(to.fullPath);
     next();
+  });
+
+  /* v-metrika directive */
+  Vue.directive('metrika', {
+    bind(el, identifier) {
+      el.addEventListener('click', () => yaMetrika.reachGoal(identifier.value));
+    },
   });
 };
