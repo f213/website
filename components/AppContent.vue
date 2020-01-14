1cded468a0a6f81ba26218b4d00addf4cc599b44
Vladimir Taranovskiy <8756136@gmail.com>
2020-01-14 12:22:45 +0300

    Added gallery`s carouseles in post page. (#17)

--- components/AppContent.vue
+++ components/AppContent.vue
@@ -2,9 +2,14 @@
   <div class="content app-content" v-html="content" />
 </template>
 <script>
+import galleryCarousel from '~/helpers/galleryCarousel';
+
 export default {
   props: {
     content: { type: String, required: true },
+  },
+  mounted() {
+    galleryCarousel.init();
   },
 };
 </script>
