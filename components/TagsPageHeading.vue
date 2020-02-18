d406937e62114a48832d5ec182f57751b5ebb681
Vladimir Taranovskiy <8756136@gmail.com>
2020-02-18 23:19:30 +0300

    Moved TagsPageHeading component to tag page. Deleted unnecessary test.

--- components/TagsPageHeading.vue
+++ components/TagsPageHeading.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="tags-page-heading">
     <h1 class="tags-page-heading__title title is-2">Заметки с тегом "{{ tag.name }}"</h1>
+
+    <HtmlComment v-if="noIndex" text="noindex" />
+
     <div class="tags-page-heading__desc app-content" v-html="tag.description"/>
+
+    <HtmlComment v-if="noIndex" text="/noindex" />
   </div>
 </template>
 
 <script>
+import HtmlComment from '~/components/HtmlComment.vue';
 
 export default {
+  components: {
+    HtmlComment,
+  },
   props: {
     tag: {
       type: Object,
       default: () => ({ name: '', description: '' }),
     },
+    noIndex: { type: Boolean, default: false },
   },
 };
 </script>
