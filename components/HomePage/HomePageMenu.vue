d7910882aa846ca9fd71ad963c745eef08d0b2a6
Fedor Borshev <fedor@borshev.com>
2020-04-13 11:43:29 +0300

    Courses address change

--- components/HomePage/HomePageMenu.vue
+++ components/HomePage/HomePageMenu.vue
@@ -4,7 +4,7 @@
       <span class="app-nav__label">Блог</span>
     </nuxt-link>
     <TgLink class="app-nav__link" label-class="app-nav__label" label="Телеграм" />
-    <a href="https://pmdaily.ru" class="app-nav__link" target="_blank">
+    <a href="https://education.borshev.com" class="app-nav__link" target="_blank">
       <span class="app-nav__label">Курсы</span>
     </a>
     <nuxt-link to="/consulting/" class="app-nav__link">
