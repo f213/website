fcaedfbe74ec33fc75b1e8d9334285be103f28ca
Fedor Borshev <fedor@borshev.com>
2020-04-26 19:43:15 +0300

    Fixed time-vulnerable tests

--- tests/helpers/datetime.test.js
+++ tests/helpers/datetime.test.js
@@ -2,7 +2,7 @@ import { fromNow } from '~/helpers/datetime';
 
 describe('fromNow helper', () => {
   it('Actualy works', () => {
-    expect(fromNow('1999-10-01T12:30:00.000Z')).toContain('лет');
+    expect(fromNow('1999-10-01T12:30:00.000Z')).toMatch(/лет|год/);
   });
   it('Does not contain NaN', () => {
     expect(fromNow('1999-10-01T12:30:00.000Z')).not.toContain('NaN');
