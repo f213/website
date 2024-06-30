ad47a16e77d437ebff4398fec27de028c4d51c0f
Fedor Borshev <fedor@borshev.com>
2024-06-30 13:19:30 +0300

    Stub blog post page

--- routes/page.js
+++ routes/page.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const ghost = require("../lib/ghost");
+
 const router = express.Router();
 
-router.get("/:slug", function (req, res, next) {
+router.get("/:slug", async (req, res, next) => {
   const { slug } = req.params;
-  if (slug === "test") {
-    res.status(200).send(slug);
-  } else {
-    next();
+  let post;
+  try {
+    post = await ghost.get(`/api/v2/content/posts/slug/${slug}/`);
+  } catch {
+    return await next();
   }
+
+  if (post.status != 200) {
+    return await next();
+  }
+  res.status(200).send(post.data);
 });
 
 module.exports = router;
