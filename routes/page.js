ce3357d34a59798214aa21d684703bdd0e6a33a3
Fedor Borshev <fedor@borshev.com>
2024-07-02 21:31:21 +0300

    Almost ready post view

--- routes/page.js
+++ routes/page.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const { format } = require("timeago.js");
 const ghost = require("../lib/ghost");
 
 const router = express.Router();
 
 router.get("/:slug", async (req, res, next) => {
-  const url = `/api/v2/content/posts/slug/${req.params.slug}/`;
+  const url = `/api/v2/content/posts/slug/${req.params.slug}/?include=tags`;
   const data = await ghost.get({ url, req });
 
-  res.status(200).render("page", { post: data.posts[0] });
+  const post = data.posts[0];
+  post.time_ago = format(post.created_at, "ru");
+
+  res.status(200).render("page", { post });
 });
 
 module.exports = router;
