19a789885bf45e56eb2a2bb4509b0223b8c74266
Fedor Borshev <fedor@borshev.com>
2024-07-03 15:00:05 +0300

    Russian timeago in the posts

--- routes/page.js
+++ routes/page.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { format } = require("timeago.js");
+const format = require("../lib/format");
 const ghost = require("../lib/ghost");
 
 const router = express.Router();
@@ -8,10 +8,7 @@ router.get("/:slug", async (req, res, next) => {
   const url = `/api/v2/content/posts/slug/${req.params.slug}/?include=tags`;
   const data = await ghost.get({ url, req });
 
-  const post = data.posts[0];
-  post.time_ago = format(post.created_at, "ru");
-
-  res.status(200).render("page", { post });
+  res.status(200).render("page", { post: format(data.posts[0]) });
 });
 
 module.exports = router;
