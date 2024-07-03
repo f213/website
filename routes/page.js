990ef11de5987a5e83d0f8c64090a673747bee9a
Fedor Borshev <fedor@borshev.com>
2024-07-03 20:42:18 +0300

    Similar posts

--- routes/page.js
+++ routes/page.js
@@ -6,11 +6,18 @@ const router = express.Router();
 
 router.get("/:slug", async (req, res, next) => {
   const url = `/api/v2/content/posts/slug/${req.params.slug}/?include=tags`;
-  const data = await ghost.get({ url, req });
+  const post = (await ghost.get({ url, req })).posts[0];
+  const similar = (
+    await ghost.get({
+      url: `/api/v2/content/posts/?tag=${post.primary_tag.slug}&limit=10`,
+      req,
+    })
+  ).posts;
 
   res.status(200).render("page", {
     route: "blog",
-    post: format(data.posts[0]),
+    post: format(post),
+    similar,
   });
 });
 
