ae29388b2c16b2cdf6744cec7b4b221880340b55
Fedor Borshev <fedor@borshev.com>
2024-07-16 21:19:58 +0300

    SEO: jsonld

--- routes/page.js
+++ routes/page.js
@@ -6,7 +6,7 @@ const { format } = require("../lib/posts");
 const router = express.Router();
 
 router.get("/:slug", async (req, res, next) => {
-  const url = `/api/v2/content/posts/slug/${req.params.slug}/?include=tags`;
+  const url = `/api/v2/content/posts/slug/${req.params.slug}/?include=tags&formats=html,plaintext`;
   const post = (await ghost.get({ url, req })).posts[0];
   const similar = (
     await ghost.get({
