7b7b13e659a9dfd1b5c28accd3e50f5895bb26a8
Fedor Borshev <fedor@borshev.com>
2024-07-16 21:37:09 +0300

    Page support

--- routes/page.js
+++ routes/page.js
@@ -6,19 +6,31 @@ const { format } = require("../lib/posts");
 const router = express.Router();
 
 router.get("/:slug", async (req, res, next) => {
-  const url = `/api/v2/content/posts/slug/${req.params.slug}/?include=tags&formats=html,plaintext`;
-  const post = (await ghost.get({ url, req })).posts[0];
-  const similar = (
-    await ghost.get({
-      url: `/api/v2/content/posts/?tag=${post.primary_tag.slug}&limit=10`,
-      req,
-      cache: true,
-    })
-  ).posts;
+  let post, url;
+
+  try {
+    url = `/api/v2/content/posts/slug/${req.params.slug}/?include=tags&formats=html,plaintext`;
+    post = (await ghost.get({ url, req })).posts[0];
+    post.is_page = false;
+  } catch {
+    url = `/api/v2/content/pages/slug/${req.params.slug}/?formats=html,plaintext`;
+    post = (await ghost.get({ url, req })).pages[0];
+    post.is_page = true;
+  }
+
+  const similar = post.primary_tag
+    ? (
+        await ghost.get({
+          url: `/api/v2/content/posts/?tag=${post.primary_tag.slug}&limit=10`,
+          req,
+          cache: true,
+        })
+      ).posts
+    : [];
 
   res.append("Last-Modified", new Date(post.updated_at).toUTCString());
   res.status(200).render("page", {
-    route: "blog",
+    route: post.is_page ? "page" : "blog",
     post: format(post),
     similar,
   });
