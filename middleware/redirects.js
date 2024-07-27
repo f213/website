be719d9948229cceee6b950a53b7d6a972132a64
Fedor Borshev <fedor@borshev.com>
2024-07-27 22:11:57 +0300

    Some old redirects

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -2,6 +2,8 @@ const routes = [
   { from: "/tags/favorites/", to: "/featured/" },
   { from: "/rss/index.xml", to: "/rss/" },
   { from: "/tough-dev-school-lms/", to: "/tough-dev-lms/" },
+  { from: "/tdd-landing/", to: "https://tough-dev.school/python-testing" },
+  { from: "/async-architecture/", to: "https://tough-dev.school/architecture" },
 ];
 
 module.exports = (req, res, next) => {
